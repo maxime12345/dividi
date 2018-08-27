255bba22fa9e7178a887512de4fd948299ada314
JeanBaptiste Bonin <jeanbaptiste.bonin@gmail.com>
2018-08-27 18:01:59 +0200

    Close #13 we can now create new items and add photos

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
     @items = current_user.items
   end
 
   def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    authorize(@item)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:id])
   end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :photo)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :collection
   monetize :price_cents
+  mount_uploader :photo, PhotoUploader
 end
