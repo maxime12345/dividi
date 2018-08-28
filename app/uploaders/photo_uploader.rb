2bdaf6d75a31d40eef1c44a68072851a22d9cc1a
JeanBaptiste Bonin <jeanbaptiste.bonin@gmail.com>
2018-08-28 17:58:49 +0200

    minor correction

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,7 @@
 # app/uploaders/photo_uploader.rb
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   process :resize_to_limit => [500, nil]
 
