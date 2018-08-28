bd1872cfe68c296da09ab943f49a5b71f82d4eea
thibaultvillard <thibault.villard@gmail.com>
2018-08-28 15:44:30 +0200

    Change status of one object

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:index, :show, :new, :create]
-  resources :items, only: [:index, :show]
-  resources :reminders, only: :index
+  resources :items, only: [:index, :show, :new, :create] do
+    resources :reminders, only: [:new, :create]
+  end
+  resources :reminders, only: [:index, :destroy]
 
 end
