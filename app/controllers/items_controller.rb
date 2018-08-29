5a3404f4e4291a61b928bc9c72b8ffa562cb24c0
JeanBaptiste Bonin <jeanbaptiste.bonin@gmail.com>
2018-08-29 12:03:45 +0200

    Mise en place basic du search
    First trick is working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,9 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    if params[:query].present?
+      @items = Item.search_by_name("#{params[:query]}")
+    end
   end
 
   def show
