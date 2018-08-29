593ed3538b590717bbd75f5b6f741185e5e5eac5
thibaultvillard <thibault.villard@gmail.com>
2018-08-29 17:01:38 +0200

    Adding pictures and seed

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,6 +3,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   # include CarrierWave::MiniMagick
 
+  def public_id
+    if model.is_a?(Item)
+      "dividi/#{model.class}/#{model.name}"
+    else
+      "dividi/#{model.class}/#{model.id}"
+    end
+  end
+
   process :resize_to_limit => [500, nil]
 
 end
