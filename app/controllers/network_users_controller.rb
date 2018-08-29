78c2a3e4dda114b84051d937f25c776276ad5d82
thibaultvillard <thibault.villard@gmail.com>
2018-08-29 11:41:25 +0200

    Adding grid in my networks

--- app/controllers/network_users_controller.rb
+++ app/controllers/network_users_controller.rb
@@ -1,6 +1,7 @@
 class NetworkUsersController < ApplicationController
   def index
     @networks = current_user.networks
+    @all_users = User.all.select{ |user| current_user != user }
   end
 
   def show
