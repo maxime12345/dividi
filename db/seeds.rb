cf50cb22d5c5d3afae7487c1fb10473406e895d7
thibaultvillard <thibault.villard@gmail.com>
2018-08-30 15:19:28 +0200

    CSS on my networks

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,8 @@ puts "Creating users and collections..."
 
 
 
-  user_second = User.create(email: "sl@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/sarah.png"))
-  user_third = User.create(email: "jbb@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/jb.jpeg"))
+  user_second = User.create(username: "Sarah", email: "sl@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/sarah.png"))
+  user_third = User.create(username: "JB", email: "jbb@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/jb.jpeg"))
   user_fourth = User.create(email: "cr@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/christine.png"))
   user5 = User.create(email: "sebastien.patoche@dividi.fr", password: '123456', password_confirmation: '123456', avatar: File.open("#{Rails.root}/app/assets/images/images_seed/patoche.jpg"))
   user6 = User.create(email: "paul.dupont@dividi.fr", password: '123456', password_confirmation: '123456')
@@ -45,10 +45,18 @@ puts "Creating networks and links..."
 
   # User 1 have all people as friends, and 1 friend in a special network
   link1 = NetworkUser.create(user: user_second, network: user_first.networks.where(name: "Tous")[0])
-  link2 = NetworkUser.create(user: user_third, network: user_first.networks.where(name: "Tous")[0])
-  link3 = NetworkUser.create(user: user_fourth, network: user_first.networks.where(name: "Tous")[0])
+  link1reverse = NetworkUser.create(user: user_first, network: user_second.networks.where(name: "Tous")[0])
 
-  link4 = NetworkUser.create(user: user_second, network: user_first.networks.where(name: "Amis")[0])
+  link2 = NetworkUser.create(user: user_third, network: user_first.networks.where(name: "Tous")[0])
+  link2reverse = NetworkUser.create(user: user_first, network: user_third.networks.where(name: "Tous")[0])
+
+  link3 = NetworkUser.create(user: user_fourth, network: user_first.networks.where(name: "Tous")[0])
+  link3reverse = NetworkUser.create(user: user_first, network: user_fourth.networks.where(name: "Tous")[0])
+
+  link4 = NetworkUser.create(user: user7, network: user_first.networks.where(name: "Tous")[0])
+  link4reverse = NetworkUser.create(user: user_first, network: user7.networks.where(name: "Tous")[0])
+
+  link5 = NetworkUser.create(user: user_second, network: user_first.networks.where(name: "Amis")[0])
 
 
 
