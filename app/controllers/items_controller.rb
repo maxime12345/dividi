0b57987f080e3f0e82d67c2135375d92cd5b2094
JeanBaptiste Bonin <jeanbaptiste.bonin@gmail.com>
2018-08-31 11:44:51 +0200

    fixing create bug

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :price, :photo, :collection_id)
+    params.require(:item).permit(:name, :price, :photo, :collection_id, :category_id)
   end
 
 end
