d37b1acf39c3e2c154db8f5a8c43604d30d52c69
thibaultvillard <thibault.villard@gmail.com>
2018-09-02 18:17:12 +0200

    Adding retrieve from a personal network

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -6,6 +6,7 @@ class NetworksController < ApplicationController
   def update_somebody_in_network
     @network = Network.find(params[:id])
     @network_user = NetworkUser.new(user_id: params[:network][:network_users], network: @network)
+    raise
     if @network_user.save
       redirect_to network_users_path
     else
