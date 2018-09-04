67162999e4c0988dff4761ef0920475fa0c900fd
christine royer <royerchrist@gmail.com>
2018-09-04 12:09:54 +0200

    view mardi midi

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_09_03_140113) do
 
-
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
@@ -96,7 +93,6 @@ ActiveRecord::Schema.define(version: 2018_09_03_140113) do
     t.string "avatar"
     t.string "username"
     t.string "email_for_search"
-    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
