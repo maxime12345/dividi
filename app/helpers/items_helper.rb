8db5f00d5a6f11ea88e5487bc6433b9e642f252e
thibaultvillard <thibault.villard@gmail.com>
2018-09-12 14:57:17 +0200

    Improve show view with all buttons

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,7 +3,7 @@ module ItemsHelper
     if item.photo.present?
       cl_image_tag item.photo, alt: "#{item.name}", class: "image-item"
     else
-      image_tag("fallback/default-picture.png")
+      image_tag("fallback/default-picture.png", class: "fallback")
     end
   end
 
@@ -15,43 +15,84 @@ module ItemsHelper
     end
   end
 
-  # return the display
-  def status(item)
-    # if no reminder
+  # retourne un hash avec toutes les informations pour les vues
+  def hash_status(item)
+    status = {text: "", icon: "", button: nil, renter: nil}
+    # Si item fantome
     if item.id.nil?
-      '<i class="fas fa-user-circle"></i> Je l\'emprunte'.html_safe
-    elsif item.reminders.size.zero?
-      '<i class="far fa-check-circle"></i> Disponible'.html_safe
-    else
-        # if reminder(s)
+      status[:text] = "Je l\'emprunte"
+      status[:icon] = "fas fa-user-circle"
 
-      if !item.reminders.where(status: nil).size.zero? # status : nil
+    # Si pas de reminder
+    elsif item.reminders.size.zero?
+      status[:text] = "Disponible"
+      status[:icon] = "far fa-check-circle"
+      if item.collection.user == current_user
+        if item.verbe == "To Rent" || item.verbe == "To Lend"
+          status[:button] = "declare"
+        end
+      else
+        status[:button] = "notifier"
+      end
+    else
+
+      # si reminder avec le statut nil (réservation validée)
+      if !item.reminders.where(status: nil).size.zero?
+        # Si l'item appartient à l'utilisateur
         if item.collection.user == current_user
+          status[:icon] = "far fa-times-circle"
+          status[:button] = "available again"
+          status[:renter] = item.reminders.where(status: nil)[0].user
           if item.verbe == "To Rent"
-            '<i class="far fa-times-circle"></i> En cours de location'.html_safe
+            status[:text] = "En cours de location"
           else
-            '<i class="far fa-times-circle"></i> En cours de prêt'.html_safe
+            status[:text] = "En cours de prêt"
           end
         elsif item.reminders[0].user == current_user
+          status[:icon] = "fas fa-user-circle"
           if item.verbe == "To Rent"
-            '<i class="fas fa-user-circle"></i> Je le loue'.html_safe
+            status[:text] = "Je le loue"
           else
-            '<i class="fas fa-user-circle"></i> Je l\'emprunte'.html_safe
+            status[:text] = "Je l\'emprunte"
           end
         else
-          '<i class="far fa-times-circle"></i> Indisponible'.html_safe
+          status[:text] = "Indisponible"
+          status[:icon] = "far fa-times-circle"
         end
 
       else # status "pending"
         if item.collection.user == current_user
-          '<i class="far fa-question-circle"></i> Demandes en cours'.html_safe
+          status[:text] = "Demandes en cours"
+          status[:icon] = "far fa-question-circle"
         elsif !item.reminders.where(user: current_user).size.zero?
-          '<i class="far fa-question-circle"></i> Attente de retour'.html_safe
+          status[:text] = "Attente de retour"
+          status[:icon] = "far fa-question-circle"
+          status[:button] = "cancel"
         else
-          '<i class="far fa-check-circle"></i> Disponible'.html_safe
-
+          status[:text] = "Disponible"
+          status[:icon] = "far fa-check-circle"
+          status[:button] = "notifier"
         end
       end
+    end
+  return status
+  end
+
+  def button(verbe)
+    if verbe == "declare"
+      link_to "Déclarer", new_item_reminder_path(params[:id]), class: "btn btn-primary"
+    elsif verbe == "notifier"
+      link_to "Envoyer une notification", item_reminders_path(@item,
+      reminder: {user_id: current_user, status: "pending"}),
+      method: :post, class: "btn btn-primary"
+    elsif verbe == "available again"
+      link_to "Objet rendu", reminder_path(@item.reminders[0].id), class: "btn btn-primary",
+      method: :delete,
+      data: {confirm: "Etes-vous à nouveau en possession de cet objet ?" }
+    elsif verbe == "cancel"
+      link_to "Annuler ma notification", reminder_path(@item.reminders[0].id), class: "btn btn-primary",
+      method: :delete,
+      data: {confirm: "Etes-vous sûr d'annuler cette notification ?" }
     end
   end
 end
