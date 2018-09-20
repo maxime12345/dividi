37ffc79cbf3a77dddaab92ed73822d0679e3de6d
thibaultvillard <thibault.villard@gmail.com>
2018-09-20 12:14:12 +0200

    Change of reminders when deleting user

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,6 +1,6 @@
 class Network < ApplicationRecord
   belongs_to :user
-  has_many :network_users
+  has_many :network_users, dependent: :destroy
   has_many :shares, dependent: :destroy
 
   has_many :validate_network_users, ->{where(status: nil)}, class_name: 'NetworkUser'
