4548003b47fbb59f809c4f53b340472e11a94ea5
Maxime Personnic <maxime.personnic@hotmail.fr>
2018-10-18 11:23:24 +0200

    setup routes for I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,47 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'confirmations' }
-  root to: 'pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope'(:locale)', locale: /fr/ do
+    root to: 'pages#home'
 
-  resources :collections, only: [:index, :show, :create, :edit, :update, :destroy]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-    resources :reminders, only: [:new, :create] do
-      collection do
-        get :new_outside
-        post :create_outside
+    resources :collections, only: [:index, :show, :create, :edit, :update, :destroy]
+
+    resources :items, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+      resources :reminders, only: [:new, :create] do
+        collection do
+          get :new_outside
+          post :create_outside
+        end
       end
     end
-  end
 
-  resources :reminders, only: [:destroy, :index] do
-    collection do
-      get :new_item_outside
-      post :create_item_outside
+    resources :reminders, only: [:destroy, :index] do
+      collection do
+        get :new_item_outside
+        post :create_item_outside
+      end
+      member do
+        get 'accept', to: 'reminders#accept'
+        delete 'decline', to: 'reminders#decline'
+      end
     end
-    member do
-      get 'accept', to: 'reminders#accept'
-      delete 'decline', to: 'reminders#decline'
+
+    resources :network_users, only: [:index, :destroy] do
+      member do
+        get :accept
+        delete :destroy_all_links
+      end
     end
-  end
 
-  resources :network_users, only: [:index, :destroy] do
-    member do
-      get :accept
-      delete :destroy_all_links
+    resources :networks, only: [:show, :update] do
+      resources :network_users, only: :create
     end
-  end
 
-  resources :networks, only: [:show, :update] do
-    resources :network_users, only: :create
+    get 'pages/dashboard', to: 'pages#dashboard'
+    get 'pages/:token', to: 'pages#user_page', as: :user_page
   end
-
-  get 'pages/dashboard', to: 'pages#dashboard'
-  get 'pages/:token', to: 'pages#user_page', as: :user_page
 
 end
 
