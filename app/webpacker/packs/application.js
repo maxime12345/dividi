9986b3d37e93a200b3ac0cfb18064dac957c006c
thibaultvillard <thibault.villard@gmail.com>
2018-10-22 13:45:04 +0200

    WIP

---
+++ app/webpacker/packs/application.js
@@ -0,0 +1,15 @@
+import "../stylesheets/application.scss"
+
+import "bootstrap";
+import 'bootstrap/dist/js/bootstrap';
+
+import '../src/javascript/accordeon';
+import '../src/javascript/animation';
+import '../src/javascript/cable';
+import '../src/javascript/search';
+
+// rails ujs
+import Rails from 'rails-ujs'
+Rails.start();
+
+console.log('Hello');
