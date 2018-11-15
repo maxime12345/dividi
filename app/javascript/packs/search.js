3fff380a4dc518763e9c53c1786fe3214b256187
thibaultvillard <thibault.villard@gmail.com>
2018-11-15 14:31:49 +0100

    WIP

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,8 +1,7 @@
 const form = document.querySelector('#filter-form');
 
-
 if (form) {
   console.log('jb');
   form.addEventListener('change', function() {console.log('changed')});
   form.addEventListener('change', function() { form.submit() });
-}
+};
