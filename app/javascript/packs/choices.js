5f155b50083791469cb8a258a897654d04bd2c7f
thibaultvillard <thibault.villard@gmail.com>
2018-11-19 09:08:40 +0100

    WIP form item index

--- app/javascript/packs/choices.js
+++ app/javascript/packs/choices.js
@@ -1,7 +1,7 @@
 import Choices from "choices.js";
 
 if ( document.querySelector("#no_model_fields_category") && document.querySelector("#no_model_fields_verbe")) {
-  new Choices("#no_model_fields_category", {
+  const choices_category = new Choices("#no_model_fields_category", {
     removeItems: true,
     removeItemButton: true,
   });
@@ -11,9 +11,13 @@ if ( document.querySelector("#no_model_fields_category") && document.querySelect
     removeItemButton: true,
   });
 
+  new Choices("#test", {
+    removeItems: true,
+    removeItemButton: true,
+  });
+
   document.querySelectorAll('select.choices__input.is-hidden').forEach(function(element) {
     element.style.display='none';
   });
 };
-
 
